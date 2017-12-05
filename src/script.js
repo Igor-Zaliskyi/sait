6c63ca8db837bc7010bb6411affefb786d272709
Igor <ihorzaliskyi@gmail.com>
2017-12-05 22:26:12 +0200

    commit__1

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // window.scrollTo(x,y)
 let scrolled;
 let timer;
-document.querySelector('body .button_up').onclick = () => { 
+const buttonUp = document.querySelector('body .button_up').onclick = () => { 
     scrolled = window.pageYOffset;
     scrollToTop(); 
 }
@@ -11,10 +11,25 @@ function scrollToTop(){
         scrolled = scrolled - 100; 
         timer = setTimeout(scrollToTop, 20);
     }
+    // if(scrolled < 400){
+    //     buttonUp.style.opacity = "0";
+    // }
     else{
         clearTimeout(timer);
         window.scrollTo(0,0);
     }
 }
+
+// const y = document.querySelector('body .button_up').style.opacity = "0";
+
+// function top(){
+    
+//         if (window.pageYOffset > 400){
+//         t.style.opacity = "0";
+//         }else{
+//             y.style.opacity = "1";
+//         }
+//     }
+    
 
 
